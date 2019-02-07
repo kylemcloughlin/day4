8d1c8f1bf7bbcd8d82e91100eab3ef6235dacf14
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-02-07 17:59:10 +0000

    commiting anonymous functions

--- waldo.js
+++ waldo.js
@@ -1,15 +1,23 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  console.log('find waldo')
+  arr.forEach(function(person, index){
+    console.log('foreach: ', person)
+    if(person === "Waldo") {
+      found(person, index);   // execute callback
     }
-  }
+  })
 }
 
-function actionWhenFound(i) {
-  console.log(`Found him at ${i} !`);
-
+function actionWhenFound(person,index) {
+  console.log(`Found ${person} at ${index}him!`);
 }
+
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
